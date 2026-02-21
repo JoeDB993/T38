8df99b25d63a864d588a380469a6657205bcc625
Jonie Miralda <jonie.mirc@gmail.com>
2026-02-20 18:07:32 -0600

    Pauta Examen

---
+++ PautaExamenI/app.js
@@ -0,0 +1,11 @@
+const express = requiere('express');
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.json());
+
+
+app.listen(PORT, ()=>{
+    console.log(`Escuchando en http://localhost:${PORT}/`);
+});
